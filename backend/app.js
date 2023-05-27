c32a7accd33c7e01a6b530096c09cae126c667d1
Your Name <you@example.com>
2023-05-27 12:46:21 +0530

    deployment updates

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 const path = require("path");
 
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: 'https://eshop-e-commerce-store-2.vercel.app/',
   credentials: true
 }));
 
